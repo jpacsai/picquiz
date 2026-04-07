e888b54e2d7ac19d6a844d829b33601d8ec57069
Judit Pacsai <judit.pacsai@gmail.com>
2026-04-07 14:02:33 +0200

    style: fix center outlined input labels vertically

--- src/lib/theme/components/inputlabel.ts
+++ src/lib/theme/components/inputlabel.ts
@@ -3,7 +3,13 @@ import type { Components, Theme } from '@mui/material/styles';
 const inputLabel: Components<Theme>['MuiInputLabel'] = {
   styleOverrides: {
     root: () => ({
-      minHeight: '22px'
+      minHeight: '22px',
+      '&.MuiInputLabel-outlined:not(.MuiInputLabel-shrink)': {
+        transform: 'translate(14px, 22px) scale(1)',
+      },
+      '&.MuiInputLabel-outlined.MuiInputLabel-sizeSmall:not(.MuiInputLabel-shrink)': {
+        transform: 'translate(14px, 21px) scale(1)',
+      },
     }),
   },
 };
